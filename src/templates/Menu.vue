7dfd799e8c411c4c70734ccc01a017dd036d2c24
Anna Mikhailova <petunova@gmail.com>
2024-10-03 13:59:28 +0100

    Add: menu product cards

--- src/templates/Menu.vue
+++ src/templates/Menu.vue
@@ -3,98 +3,31 @@
     <h2 class="menu-container-title text-dark">Behind each of our cups hides an <span class="text-accent">amazing
         surprise</span></h2>
     <div class="menu-selection">
-      <label class="menu-selection-link coffee-selection-link link-text">
-        <input type="radio" name="product-type" value="coffee" checked />
-        <span class="menu-icon coffee-menu-icon"></span>Coffee
-      </label>
-      <label class="menu-selection-link tea-selection-link link-text">
-        <input type="radio" name="product-type" value="tea" />
-        <span class="menu-icon tea-menu-icon"></span>Tea
-      </label>
-      <label class="menu-selection-link dessert-selection-link link-text">
-        <input type="radio" name="product-type" value="dessert" />
-        <span class="menu-icon dessert-menu-icon"></span>Dessert
+      <label v-for="group in groups" class="menu-selection-link link-text" :class="group + '-selection-link'">
+        <input type="radio" name="product-type" :value="group" :checked="group == 'coffee'" v-model="groupSelected" />
+        <span class="menu-icon" :class="group + '-menu-icon'"></span>{{ group }}
       </label>
     </div>
-    <ul class="cards-container coffee-cards-container">
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-1.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Irish coffee</h3>
-            <p class="product-card-description">Fragrant black coffee with Jameson Irish whiskey and whipped milk</p>
-            <h3>$7.00</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-2.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Kahlua coffee</h3>
-            <p class="product-card-description">Classic coffee with milk and Kahlua liqueur under a cap of frothed
-              milk</p>
-            <h3>$7.00</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-3.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Honey raf</h3>
-            <p class="product-card-description">Espresso with frothed milk, cream and aromatic honey</p>
-            <h3>$5.50</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-4.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Ice cappuccino</h3>
-            <p class="product-card-description">Cappuccino with soft thick foam in summer version with ice</p>
-            <h3>$5.00</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-5.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Espresso</h3>
-            <p class="product-card-description">Classic black coffee</p>
-            <h3>$4.50</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-6.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Latte</h3>
-            <p class="product-card-description">Espresso coffee with the addition of steamed milk and dense milk foam
-            </p>
-            <h3>$5.50</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-7.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Latte macchiato</h3>
-            <p class="product-card-description">Espresso with frothed milk and chocolate</p>
-            <h3>$5.50</h3>
-          </div>
-        </a></li>
-      <li class="product-card-item coffee-card-item"><a class="product-card coffee-card">
-          <img class="product-card-picture" src="../assets/img/select-coffees/coffee-8.png" alt="coffee-1">
-          <div class="product-card-content">
-            <h3 class="product-card-title">Coffee with cognac</h3>
-            <p class="product-card-description">Fragrant black coffee with cognac and whipped cream</p>
-            <h3>$6.50</h3>
-          </div>
-        </a></li>
-
-
+    <ul class="cards-container" v-if="products">
+      <ProductCard v-for="product in filteredProducts" :name="product.name" :description="product.description"
+        :category="product.category" :price="product.price"></ProductCard>
     </ul>
-
   </main>
 </template>
-<script>
-export default {
-  setup() {
-    const groups = ["coffee", "tea", "dessert"];
+<script setup>
+import { ref, computed } from 'vue'
+import ProductCard from '../templates/components/ProductCard.vue'
 
-    let groupSelected = groups[0];
-  }
-}
+const groups = ["coffee", "tea", "dessert"];
+const groupSelected = ref("coffee");
+let products = ref([]);
+let filteredProducts = computed(() => {
+  return products.value.filter((item) => item.category == groupSelected.value)
+})
+
+//Get products
+fetch('../src/assets/json/products.json').then((data) => data.json()).then((result) => {
+  products.value = result;
+});
+
 </script>
---
+++ src/templates/components/ProductCard.vue
@@ -0,0 +1,14 @@
+<template>
+ <li class="product-card-item" :class="category + '-card-item'"><a class="product-card" :class="category + '-card'">
+   <img class="product-card-picture" src="../../assets/img/select-coffees/coffee-1.png" alt="coffee-1">
+   <div class="product-card-content">
+    <h3 class="product-card-title">{{ name }}</h3>
+    <p class="product-card-description">{{ description }}</p>
+    <h3>{{ price }}</h3>
+   </div>
+  </a></li>
+</template>
+<script setup>
+const props = defineProps(['name', 'description', 'price', 'category']);
+
+</script>
